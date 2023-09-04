cf20524dadc6563ad56009969d4c7d2097d9c219
Jenia Sakirko <jenia@cycode.com>
2023-09-04 23:45:47 +0300

    Add basic hex files

---
+++ elixir_hex/mix.exs
@@ -0,0 +1,25 @@
+defmodule JeniaTest.Mixfile do
+  use Mix.Project
+
+  def project do
+    [
+
+      app: :jenia_test,
+      version: "0.1.2",
+      elixir: "~> 1.5",
+      config_path: "config/config.exs",
+      start_permanent: Mix.env == :prod,
+      deps: deps()
+    ]
+  end
+
+  def application do
+    [extra_applications: [:logger]]
+  end
+
+  defp deps do
+    [
+      {:ecto, "== 2.2.0"}
+    ]
+  end
+end
