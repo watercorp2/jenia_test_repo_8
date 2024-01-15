fa9a8ab0996e4d5ba119d24320f371e5ee63d9a4
Jenia Sakirko <jenia@cycode.com>
2024-01-15 13:54:34 +0200

    remove omg

--- omg_load/files/cloneFfiles.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-original_folder="test.txt"
-
-for ((suffix=1; suffix<=20000; suffix++)); do
-  clone_folder="test_$suffix.txt"
-  cp "$original_folder" "$clone_folder"
-  #echo "Cloned $original_folder to $clone_folder"
-done
