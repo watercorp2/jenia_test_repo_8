b72ed947d97f2ae6613e9fa7b6d048f22799341e
Jenia Sakirko <jenia@cycode.com>
2024-03-24 13:46:59 +0200

    wip

--- cloneFolder.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-original_folder="crazy_load"
-
-for ((suffix=1; suffix<=60; suffix++)); do
-  clone_folder="crazy_load_$suffix"
-  cp -r "$original_folder" "$clone_folder"
-  echo "Cloned folder $original_folder to $clone_folder"
-done
